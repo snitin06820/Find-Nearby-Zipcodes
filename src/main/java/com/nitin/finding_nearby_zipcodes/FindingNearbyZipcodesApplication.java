9574af752a61ef7854e5b1fcfe98cfd421e22bc7
Nitin Sharma <nitin_sharma@Nitins-MacBook-Air.local>
2024-09-13 19:40:40 +0530

    added starter files

---
+++ src/main/java/com/nitin/finding_nearby_zipcodes/FindingNearbyZipcodesApplication.java
@@ -0,0 +1,13 @@
+package com.nitin.finding_nearby_zipcodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindingNearbyZipcodesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindingNearbyZipcodesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nitin/finding_nearby_zipcodes/FindingNearbyZipcodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitin.finding_nearby_zipcodes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindingNearbyZipcodesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
